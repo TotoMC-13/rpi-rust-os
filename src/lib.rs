07ad364409dc7fa2e4efeded1d1597dbddfa09b6
TotoMC-13 <tomasazcaray@gmail.com>
2026-04-15 10:31:20 -0300

    testing framework

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,82 @@
 #![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use crate::uart::Uart;
+use core::panic::PanicInfo;
 
 pub mod uart;
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        Uart::init();
+        print!("{}...\t", core::any::type_name::<T>());
+        self();
+        println!("[ok]");
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    Uart::init();
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    println!("[failed]\n");
+    println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+// Punto de entrada para `cargo test`
+#[cfg(test)]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u64)]
+pub enum QemuExitCode {
+    Success = 0x20026, // ADP_Stopped_ApplicationExit
+    Failed = 0x20027,  // ADP_Stopped_RunTimeErrorUnknown
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    // AArch64 semihosting: SYS_EXIT (0x18)
+    // x0 = num de operacion (0x18)
+    // x1 = pointer al bloque de parametros: [reason: u64, sub_code: u64]
+    const SYS_EXIT: u64 = 0x18;
+
+    let params: [u64; 2] = [exit_code as u64, 0];
+
+    unsafe {
+        core::arch::asm!(
+            "hlt #0xF000",
+            in("x0") SYS_EXIT,
+            in("x1") params.as_ptr(),
+            options(nostack),
+        );
+    }
+    loop {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::arch::global_asm;
 use core::panic::PanicInfo;
@@ -16,14 +19,24 @@ pub extern "C" fn _start_rust() -> ! {
     println!("--- Kernel ARM64 Iniciado ---");
     println!("Version: {}", "0.1.0");
 
+    #[cfg(test)]
+    test_main();
+
     loop {
         let c = rpi_mini_os::uart::WRITER.lock().read_byte();
         println!("Caracter actual: {}", c as char);
     }
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     let _ = println!("[ERROR]: {info}");
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rpi_mini_os::test_panic_handler(info)
 }
